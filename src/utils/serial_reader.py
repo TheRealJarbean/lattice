8dd9d541a15258c5d7639a0edcf04d977ef67982
TheRealJarbean <jaron.e.anderson@gmail.com>
2025-10-09 20:34:59 -0400

    fix: Replace poor QThread implementation

--- src/utils/serial_reader.py
+++ src/utils/serial_reader.py
@@ -1,22 +1,26 @@
-from PySide6.QtCore import QThread, Signal, QMutex
+from PySide6.QtCore import QObject, Signal, QMutex, QTimer
 import serial
 import logging
 
 logger = logging.getLogger(__name__)
 
-class SerialReader(QThread):
+class SerialReader(QObject):
     data_received = Signal(str)
     
     def __init__(self, ser: serial.Serial, mutex: QMutex):
         super().__init__()
         self.ser = ser
         self.mutex = mutex
+        self.read_timer = QTimer(self)
+        self.read_timer.timeout.connect(self.read)
         
-    def run(self):
-        self._is_running = True
-        while self._is_running:
-            self.data_received.emit("Hello!")
-            self.msleep(1000)
+    def read(self):
+        if not self.ser.is_open:
+            logger.debug(f"Serial port {self.ser.port} is not connected, restart required")
+            self.read_timer.stop()
+            return
+        
+        try:
             if self.ser.in_waiting:
                 self.mutex.lock()
                 try:
@@ -26,8 +30,14 @@ class SerialReader(QThread):
                         self.data_received.emit(message)
                 except Exception as e:
                     print(f"Serial read error: {e}")
-                self.mutex.unlock()
-            self.msleep(10) # Let other threads run
-            
+                    self.mutex.unlock()
+        except serial.SerialException as e:
+            logger.error(e)
+    
+    def start(self):
+        if not self.read_timer.isActive():
+            self.read_timer.start(20)
+    
     def stop(self):
-        self._is_running = False
+        if self.read_timer.isActive():
+            self.read_timer.stop()
