56053f35e8cd619fbf362a2658ccaeb4204a7385
TheRealJarbean <jaron.e.anderson@gmail.com>
2025-10-17 01:14:03 -0400

    fix: Make startup scripts always execute in the right place

--- start.bat
+++ start.bat
@@ -1,4 +1,5 @@
 @echo off
+cd /d "%~dp0"
 title GT MBE Control Warnings
 set LOG_LEVEL=WARNING
 venv\Scripts\python.exe src\main.py
--- start_debug.bat
+++ start_debug.bat
@@ -1,4 +1,5 @@
 @echo off
+cd /d "%~dp0"
 title GT MBE Control Debugger
 set LOG_LEVEL=DEBUG
 venv\Scripts\python.exe src\main.py
