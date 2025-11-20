fc77c5890b9cf785b17b6a1f55996df9f283df5d
Jaron Anderson <jaron@pressanybutton.us>
2025-11-20 15:27:41 -0500

    fix: Update location and name of main scripts

--- start.bat
+++ start.bat
@@ -6,4 +6,4 @@ cd /d "%~dp0"
 title GT MBE Control Warnings
 set LOG_LEVEL=WARNING
 call venv\Scripts\activate.bat
-venv\Scripts\python.exe src\main.py
+venv\Scripts\python.exe src\mbe_software\app.py
